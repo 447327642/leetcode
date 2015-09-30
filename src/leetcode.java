d3d48f6c23995702fd1997e4efecd6869a3d3e42
byc <yuanchenbai@gmail.com>
2015-09-29 20:51:06 -0700

    leetcode

--- src/leetcode.java
+++ src/leetcode.java
@@ -4,9 +4,30 @@ public class leetcode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		addDigits a = new addDigits();
-		int b = a.addDigit(56);
+		int b = addDigits(56);
 		System.out.println(b);
 	}
-
+	//09-29
+	public static int addDigits(int num) {
+        while(num>9){
+        int a = num/10;
+        int b = num%10;
+        num = a+b;
+        }
+        return num;
+    }
+	
+	public class TreeNode {
+	    int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+    //09-29
+	public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int a = maxDepth(root.left);
+        int b = maxDepth(root.right);
+        return 1+ Math.max(a,b);
+        }
 }
